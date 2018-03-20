4c12d4bf55edecfb28c0ae2a13c9fb66333b0c74
Patrick Monteiro <eng.patrickmonteiro@gmail.com>
2018-03-20 00:26:18 -0300

    acessando o array de paciente e calculando o imc

--- js/principal.js
+++ js/principal.js
@@ -1,33 +1,39 @@
 var titulo = document.querySelector('.titulo');
 titulo.textContent = "Patrick Monteiro";
 
-var paciente = document.querySelector("#primeiro-paciente");
+var pacientes = document.querySelectorAll(".paciente");
 
-var tdPeso = paciente.querySelector(".info-peso");
-var peso = tdPeso.textContent;
+for(var i = 0; i < pacientes.length; i++ ){
+  var paciente = pacientes[i];
 
-var tdAltura = paciente.querySelector(".info-altura");
-var altura = tdAltura.textContent;
+  var tdPeso = paciente.querySelector(".info-peso");
+  var peso = tdPeso.textContent;
 
-// selecionando a coluna do IMC
-var tdImc = paciente.querySelector(".info-imc");
+  var tdAltura = paciente.querySelector(".info-altura");
+  var altura = tdAltura.textContent;
 
-var pesoEhValido = true;
-var alturaEhValida = true;
+  // selecionando a coluna do IMC
+  var tdImc = paciente.querySelector(".info-imc");
 
-if(peso <= 0 || peso >= 1000) {
-  console.log("Peso é inválido");
-  pesoEhValido = false;
-  tdImc.textContent = "Peso Inválido";
+  var pesoEhValido = true;
+  var alturaEhValida = true;
+
+  if(peso <= 0 || peso >= 1000) {
+    console.log("Peso é inválido");
+    pesoEhValido = false;
+    tdImc.textContent = "Peso Inválido";
+  }
+
+  if(altura<= 0 || altura >= 3.00) {
+    cosole.log("Altura é inválida");
+    alturaEhValida = false;
+    tdImc.textContent = "Altura Inválida";
+  }
+
+  if( alturaEhValida && pesoEhValido) {
+    var imc = peso / (altura * altura);
+    tdImc.textContent = imc.toFixed(2);
+  }
+
 }
 
-if(altura<= 0 || altura >= 3.00) {
-  cosole.log("Altura é inválida");
-  alturaEhValida = false;
-  tdImc.textContent = "Altura Inválida";
-}
-
-if( alturaEhValida && pesoEhValido) {
-  var imc = peso / (altura * altura);
-  tdImc.textContent = imc;
-}
