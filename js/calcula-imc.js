7453debdac5ee1b6d42cec0568a51ebb5b4e04a0
Patrick Monteiro <eng.patrickmonteiro@gmail.com>
2018-03-21 23:26:15 -0300

    Capitulo 5, refatoração do código em funções menores

---
+++ js/calcula-imc.js
@@ -0,0 +1,51 @@
+var titulo = document.querySelector('.titulo');
+titulo.textContent = "Patrick Monteiro";
+
+var pacientes = document.querySelectorAll(".paciente");
+
+for(var i = 0; i < pacientes.length; i++ ){
+  var paciente = pacientes[i];
+
+  var tdPeso = paciente.querySelector(".info-peso");
+  var peso = tdPeso.textContent;
+
+  var tdAltura = paciente.querySelector(".info-altura");
+  var altura = tdAltura.textContent;
+
+  // selecionando a coluna do IMC
+  var tdImc = paciente.querySelector(".info-imc");
+
+  var pesoEhValido = true;
+  var alturaEhValida = true;
+
+  if(peso <= 0 || peso >= 1000) {
+    console.log("Peso é inválido");
+    pesoEhValido = false;
+    tdImc.textContent = "Peso Inválido";
+    paciente.classList.add("paciente-invalido");
+  }
+
+  if(altura<= 0 || altura >= 3.00) {
+    console.log("Altura é inválida");
+    alturaEhValida = false;
+    tdImc.textContent = "Altura Inválida";
+     paciente.classList.add("paciente-invalido");
+  }
+
+  if( alturaEhValida && pesoEhValido) {
+    var imc = calculaIMC(peso, altura);
+    tdImc.textContent = imc;
+  }
+
+}
+
+function calculaIMC(peso, altura){
+  var imc = 0;
+
+  imc = peso / (altura * altura);
+
+  return imc.toFixed(2);
+
+}
+
+
